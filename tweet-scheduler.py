cb3e8097cfdaa1ba27538479bf1e273dcf7e0145
Claudio Canales <klaudioz@gmail.com>
2021-02-09 14:47:09 -0700

    Get secrets properly from python script

--- tweet-scheduler.py
+++ tweet-scheduler.py
@@ -1,8 +1,16 @@
+import os
 from twitter import *
 from airtable import airtable
 
-t = Twitter(auth=OAuth(TOKEN, TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
+# Get secrets from environment variables
+TOKEN= os.environ.get('TOKEN')
+TOKEN_SECRET = os.environ.get('TOKEN_SECRET')
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET= os.environ.get('CONSUMER_SECRET')
+AIRTABLE_BASE = os.environ.get('AIRTABLE_BASE')
+AIRTABLE_KEY = os.environ.get('AIRTABLE_KEY')
 
+t = Twitter(auth=OAuth(TOKEN, TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
 at = airtable.Airtable(AIRTABLE_BASE, AIRTABLE_KEY)
 
 result = at.get(table_name='Tweets',max_records=1,view="Today")
